7e96a01c8b378154f9f4fa685be5ac82296fa6fc
Blue-worker-vivant <vivant.nozaki1001@gmail.com>
2026-08-04 17:26:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function goWolfSelect(){
+ // 最初の画面を消す
+ document.getElementById("start-screen").style.display = "none";
 
+ // ウルフ画面を表示
+ document.getElementById("wolf-screen").style.display = "block";
+}
