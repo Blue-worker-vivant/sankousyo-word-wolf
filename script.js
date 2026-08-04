affd78dd4b3dfe6f2fcf4e6e892343f1f8088b4c
Blue-worker-vivant <vivant.nozaki1001@gmail.com>
2026-08-04 19:32:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -43,5 +43,5 @@ function updatePeople(){
  const citizens = players - wolves;
 
  document.getElementById("people-info").textContent =
-  "市民：" + citizens + "人  おのたや：" + wolves + "人";
+  "おのたや：" + citizens + "人  おのたや：" + wolves + "人";
 }
