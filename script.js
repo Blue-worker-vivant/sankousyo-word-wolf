4ddcc1789dc1fb2e1dd3075325cb05dbaf17f953
Blue-worker-vivant <vivant.nozaki1001@gmail.com>
2026-08-04 18:37:10 +0900

    Correct innerHTML reset and enhance comments
    Fix typo in wolfSelect innerHTML reset and improve comments.

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 function goWolfSelect(){
+ // 選んだ人数を取得
+ const playersText = document.getElementById("players").value;
+
+ //「5人」→5に変換
+ const players = Number(playersText.replace("人",""));
+ 
  // 最初の画面を消す
  document.getElementById("start-screen").style.display = "none";
 
  // ウルフ画面を表示
  document.getElementById("wolf-screen").style.display = "block";
+
+ // ウルフ選択肢を作る
+ const wolfSelect = document.getElementById("wolf-count");
+
+ // 一度リセット
+ wolfSelect.InnerHTML = "";
+
+ // 1人〜n-2人まで追加
+ for(let i = 1; i <= players - 2; i++){
+  const option = document.createElement("option");
+  option.textContent = i + "人";
+  wolfSelect.appendChild(option);
+ }
+
+ // 最初の人数表示
+ updatePeople();
+}
+
+// 市民人数表示
+function updatePeople(){
+
+ const playersText = document.getElementById("players").value;
+ const players = Number(playersText.replace("人",""));
+
+ const wolfText = document.getElementById("wolf-count").value;
+ const wolves = Number(wolfText.replace("人",""));
+
+ const citizens = players - wolves;
+
+ document.getElementById("people-info").textContent =
+  "市民：" + citizens + "人  ウルフ：" + wolves + "人";
 }
